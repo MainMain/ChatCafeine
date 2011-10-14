455acfcecca945d07254c76138d1a6cbdab97b82
Johan <Johan.Brun.etu@univ-lemans.fr>
2011-10-15 00:06:22 +0200

    Travail IHM

--- ChatCafeine/src/sources/client/vue/AccueilPanel.java
+++ ChatCafeine/src/sources/client/vue/AccueilPanel.java
@@ -1,25 +1,71 @@
 package sources.client.vue;
 
-import com.google.gwt.user.client.ui.DockPanel;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.LayoutPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.*;
 
-public class AccueilPanel extends DockPanel{
-
+public class AccueilPanel extends AbsolutePanel{
+	public VerticalPanel mainPanel;
+	public VerticalPanel headPanel;
+	public HorizontalPanel bodyPanel;
+	
 	public AccueilPanel(){
-		Label test = new Label("Coucou !");
-		add(test);
-		setSize("1400px", "587px");
+		//setWidth("99%");
 		setStyleName("accueilPanel");
+
+		mainPanel = new VerticalPanel();
+		mainPanel.setHorizontalAlignment(mainPanel.ALIGN_CENTER);
+		//mainPanel = new VerticalPanel();
+		mainPanel.setBorderWidth(1);
+		mainPanel.setWidth("100%");
+		mainPanel.setHeight("100%");
+		mainPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
+		
+		createHeadPanel();
+		createBodyPanel();
+		createLeftPanel();
+		createRightPanel();
+		
+		mainPanel.add(headPanel);
+		mainPanel.add(bodyPanel);
+		add(mainPanel);
 		
 		/*
 		Image cafeImg = new Image("img/Accueil.gif");
 		VerticalPanel panelLeft = new VerticalPanel();
 		panelLeft.setSpacing(10);
-		add(panelLeft, DockPanel.WEST);
+		add(panelLeft, mainPanel.WEST);
 		panelLeft.add(cafeImg);
 		*/
 	}
+	private void createHeadPanel() {
+		headPanel = new VerticalPanel();
+		headPanel.setWidth("99%");
+		headPanel.setHeight("100%");
+		headPanel.setBorderWidth(1);
+		headPanel.setStyleName("headPanel");
+		
+		headPanel.add(new HTML("CoucouHead"));
+	}
+
+	private void createBodyPanel() {
+		bodyPanel = new HorizontalPanel();
+		bodyPanel.setWidth("99%");
+		//bodyPanel.setHeight("200px");
+		bodyPanel.setBorderWidth(1);
+		bodyPanel.setStyleName("bodyPanel");
+		
+		bodyPanel.add(new HTML("CoucouBody"));
+	}
+
+	/**
+	 * 
+	 */
+	private void createRightPanel() {
+	}
+
+	/**
+	 * 
+	 */
+	private void createLeftPanel() {
+	}
+
 }
--- ChatCafeine/src/sources/client/vue/IHM.java
+++ ChatCafeine/src/sources/client/vue/IHM.java
@@ -9,7 +9,7 @@ public class IHM implements EntryPoint {
 	public Panel panelEnCours = null;
 	public void onModuleLoad() {
 		/*
-		 * Création des  commande s
+		 * Création des commandes
 		 */
 		Command cmd = new Command() {
 			public void execute() {
@@ -21,22 +21,35 @@ public class IHM implements EntryPoint {
 		 * Création de l'IHM
 		 */
 		Window.setMargin("0px");
+		
+		DockPanel dockPanel = new DockPanel();
 
+		//dockPanel.setHorizontalAlignment(dockPanel.ALIGN_CENTER);
+		dockPanel.setWidth("99%");
+		dockPanel.setHeight("599px");
+		dockPanel.setStyleName("mainPanelIHM");
+		RootPanel rootPanel = RootPanel.get();
+		rootPanel.add(dockPanel);
+		
 		MenuBar menu = new MenuBar();
 		menu.addItem("Fichier", cmd);
 		menu.addItem("Application", cmd);
 		menu.addItem("Aide", cmd);
 		menu.addItem("A propos", cmd);
 		menu.addItem("Se déconnecter", cmd);
+		menu.setWidth("99%");
+		menu.addStyleName("menu");
 
 		AccueilPanel accPan = new AccueilPanel();
 		panelEnCours = accPan;
-
+		panelEnCours.setStyleName("test");
 		/*
 		 * Ajouts à au RootPanel
 		 */
-		RootPanel.get().add(menu);
-		RootPanel.get().add(panelEnCours);
+		RootPanel.get().add(dockPanel);
+		dockPanel.add(menu, dockPanel.NORTH);
+		dockPanel.add(panelEnCours, dockPanel.CENTER);
+		//RootPanel.get().add(panelEnCours);
 
 	}
 
