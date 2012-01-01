d38a02ea3cecdbb24c0f9ebd1a120593b2510bbf
Johan <Johan.Brun.etu@univ-lemans.fr>
2012-01-01 21:20:45 +0100

    fix

--- ChatCafeine/src/sources/server/ProfilServiceImpl.java
+++ ChatCafeine/src/sources/server/ProfilServiceImpl.java
@@ -16,6 +16,7 @@ public class ProfilServiceImpl extends RemoteServiceServlet implements ProfilSer
 	
 	@Override
 	public boolean modifMdp(int idUser, String newMdp) {
+		System.out.println("eeeeeeeeshjhfjdshodsoj");
 		// LA METHODE SERA CODEE PAR AUDREY *********************
 		return false;
 	}
@@ -25,6 +26,7 @@ public class ProfilServiceImpl extends RemoteServiceServlet implements ProfilSer
 	@Override
 	public boolean modifInfos(int idUser, String aime, String aimePas, int age,
 			String activite) {
+		System.out.println("eeeeeeeeeeeeeeeeeeeeeeeeeeeeertoklefdkjshjhfjdshodsoj");
 		// Controle que les infos ne soient pas vides et insére que ceux complétés. (et que age > 10 et < 99)
 		// LA METHODE SERA CODEE PAR AUDREY *********************
 		return false;
