5e07567574bacb40c9899cf73e05cec0020e8914
Smallbeetle <Audrey.Feurprier.Etu@univ-lemans.fr>
2012-01-01 20:08:55 +0100

    test

--- ChatCafeine/src/sources/server/ProfilServiceImpl.java
+++ ChatCafeine/src/sources/server/ProfilServiceImpl.java
@@ -5,6 +5,7 @@ package sources.server;
 
 import sources.client.service.ProfilService;
 
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 /**
@@ -17,7 +18,18 @@ public class ProfilServiceImpl extends RemoteServiceServlet implements ProfilSer
 	@Override
 	public boolean modifMdp(int idUser, String newMdp) {
 		// LA METHODE SERA CODEE PAR AUDREY *********************
-		return false;
+		ConBDD connexion=new ConBDD();
+		String requete="UPDATE utilisateur SET Pass = "+newMdp+" WHERE ID_user = "+idUser;
+		System.out.println("TEST : requete = "+requete);
+		String resultat=connexion.setData(requete);
+		connexion.fermer();
+		if (resultat==null || resultat.equals("Error"))
+			return false;
+		else{
+			Window.alert("Votre mot de passe à bien été changé");
+			return true;
+		}
+		
 	}
 	/**
 	 * 
@@ -26,7 +38,18 @@ public class ProfilServiceImpl extends RemoteServiceServlet implements ProfilSer
 	public boolean modifInfos(int idUser, String aime, String aimePas, int age,
 			String avatar) {
 		// LA METHODE SERA CODEE PAR AUDREY *********************
-		return false;
+		
+		
+		ConBDD connexion=new ConBDD();
+		String requete="UPDATE utilisateur SET Aime = "+aime+" AND AimePas = "+aimePas+" AND Age = "+age+" AND Avatar = "+avatar+" WHERE ID_user = "+idUser;
+		String resultat=connexion.setData(requete);
+		connexion.fermer();
+		if (resultat==null || resultat.equals("Error"))
+			return false;
+		else{
+			Window.alert("Vos informations ont été changées");
+			return true;
+		}
 	}
 	/**
 	 * 
@@ -34,7 +57,16 @@ public class ProfilServiceImpl extends RemoteServiceServlet implements ProfilSer
 	@Override
 	public boolean modifDroit(int idUser, String newDroit) {
 		// LA METHODE SERA CODEE PAR AUDREY *********************
-		return false;
+		ConBDD connexion=new ConBDD();
+		String requete="UPDATE utilisateur SET Droit = "+newDroit+" WHERE ID_user = "+idUser;
+		String resultat=connexion.setData(requete);
+		connexion.fermer();
+		if (resultat==null || resultat.equals("Error"))
+			return false;
+		else {
+			Window.alert("Vos droits ont bien été changés");
+			return true;
+		}
 	}
 
 }
