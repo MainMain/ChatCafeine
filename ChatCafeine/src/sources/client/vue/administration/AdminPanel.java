87d64fa15e47890c12dedf9cd820896838cc1918
TomPouce <thomasmartinet@neuf.fr>
2012-01-03 02:05:41 +0100

    modif panelAdmin

--- ChatCafeine/src/sources/client/vue/administration/AdminPanel.java
+++ ChatCafeine/src/sources/client/vue/administration/AdminPanel.java
@@ -7,6 +7,7 @@ import java.util.List;
 import sources.client.model.Salle;
 import sources.client.model.User;
 import sources.client.service.AdminService;
+import sources.client.service.CompteService;
 import sources.client.service.SalleService;
 import sources.client.vue.Core;
 
@@ -17,7 +18,12 @@ import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
 
@@ -30,12 +36,15 @@ import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.cell.client.HasCell;
 import com.google.gwt.cell.client.NumberCell;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 
 import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
 
 
 public class AdminPanel extends AbsolutePanel{
-	
+
 	public AdminPanel(){
 		configPanel();
 	}
@@ -47,13 +56,13 @@ public class AdminPanel extends AbsolutePanel{
 		//Create a new stack layout panel.
 		StackLayoutPanel stackPanel = new StackLayoutPanel(Unit.EM);
 		stackPanel.setWidth("100%");
-		stackPanel.setHeight("575px");
+		stackPanel.setHeight("700px");
 
 		// Add the list user
 		Widget presentationPanelHeader = createHeaderWidget("Présentation de l'administration");
 		stackPanel.add(creerPresentationPanel(),presentationPanelHeader, 4);
 
-		
+
 		// Add the list user
 		Widget listUserPanelHeader = createHeaderWidget("Liste des utilisateurs");
 		stackPanel.add(creerListeUserPanel(),listUserPanelHeader, 4);
@@ -73,7 +82,7 @@ public class AdminPanel extends AbsolutePanel{
 	 */
 
 	private ArrayList<Salle> listeSalles = new ArrayList<Salle>();
-	
+
 	private Widget creerPresentationPanel(){
 		SimplePanel a = new SimplePanel();
 		setWidth("100%");
@@ -85,16 +94,16 @@ public class AdminPanel extends AbsolutePanel{
 		a.add(textFiche);
 		return a;
 	}
-	
+
 	private Widget creerListeSallePanel() {
 		SimplePanel a = new SimplePanel();
 		setWidth("100%");
 		setHeight("100%");
 		a.isVisible();
 		a.setTitle("Liste des salles");
-		
+
 		final CellTable<Salle> table = new CellTable<Salle>();
-		
+
 		// RECUPERER LES INSTANCES DES SALLES
 		SalleService.Util.getInstance().getToutesSalles(new AsyncCallback<ArrayList<Salle>>() {
 			@Override
@@ -127,7 +136,7 @@ public class AdminPanel extends AbsolutePanel{
 				System.out.println("Erreur lors de la récupération de la liste des salles");
 			}
 		});
-		
+
 		class HasCellImpl implements HasCell<Salle, Salle> {
 			private ActionCell<Salle> fCell;
 
@@ -159,26 +168,26 @@ public class AdminPanel extends AbsolutePanel{
 				Window.alert(object.getNom());
 			}
 		}));
-		
+
 		CompositeCell<Salle> principaleColumn = new CompositeCell<Salle>(cells);
 		Cell<Number> numberColumn = new NumberCell();
 		Cell<String> editTextColumn = new EditTextCell();
-		
+
 		// Create nom column.
 		table.addColumn(new Column<Salle, String>(editTextColumn) {
 			public String getValue(Salle salle) {
-	            return salle.getNom();
-	          }
+				return salle.getNom();
+			}
 		},"Nom");
-		
+
 		// Create theme column.
 		table.addColumn(new Column<Salle, String>(editTextColumn) {
 			public String getValue(Salle salle) {
-	            return salle.getTheme();
-	          }
+				return salle.getTheme();
+			}
 		},"Theme");
-		
-		
+
+
 		// Create nb place max column.
 		table.addColumn(new Column<Salle, Number>(numberColumn) {
 
@@ -187,7 +196,7 @@ public class AdminPanel extends AbsolutePanel{
 				return salle.getNbrPlaceMax();
 			}
 		},"nb Places Max");
-		
+
 		// Create bouton supprimer salle column.
 		table.addColumn(new Column<Salle, Salle>(principaleColumn) {
 
@@ -196,7 +205,7 @@ public class AdminPanel extends AbsolutePanel{
 				return object;
 			}
 		},"Suprimer Salle");
-		
+
 
 		// Add it to the root panel.
 		a.add(table);
@@ -212,41 +221,41 @@ public class AdminPanel extends AbsolutePanel{
 		setHeight("100%");
 		a.isVisible();
 		a.setTitle("Liste des utilisateurs");	
-	
+
 		AdminService.Util.getInstance().getAllUsers(new AsyncCallback<ArrayList<User>>(){
-			 
-							@Override
-							public void onFailure(Throwable caught) {
-								Window.alert("Erreur : "+ caught.getMessage());
-							}
-							@Override
-							public void onSuccess(ArrayList<User> result) {
-								if (result==null)
-									Window.alert("Erreur lors de la récupération de la liste des utilisateurs");
-								listeUtilisateurs = result;
-								// The list of data to display.
-								List<User> USERS = listeUtilisateurs;
-								// Create a data provider.
-								ListDataProvider<User> dataProvider = new ListDataProvider<User>();
 
-								// Connect the table to the data provider.
-								dataProvider.addDataDisplay(table);
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("Erreur : "+ caught.getMessage());
+			}
+			@Override
+			public void onSuccess(ArrayList<User> result) {
+				if (result==null)
+					Window.alert("Erreur lors de la récupération de la liste des utilisateurs");
+				listeUtilisateurs = result;
+				// The list of data to display.
+				List<User> USERS = listeUtilisateurs;
+				// Create a data provider.
+				ListDataProvider<User> dataProvider = new ListDataProvider<User>();
 
-								// Add the data to the data provider, which automatically pushes it to the
-								// widget.
-								List<User> list = dataProvider.getList();
-								for (User user : USERS) {
-									list.add(user);
-								}
-								// Add a ColumnSortEvent.ListHandler to connect sorting to the
-								// java.util.List.
-								ListHandler<User> columnSortHandler = new ListHandler<User>(
-										list);
-								table.addColumnSortHandler(columnSortHandler);
-							}
-								
-						});
-		
+				// Connect the table to the data provider.
+				dataProvider.addDataDisplay(table);
+
+				// Add the data to the data provider, which automatically pushes it to the
+				// widget.
+				List<User> list = dataProvider.getList();
+				for (User user : USERS) {
+					list.add(user);
+				}
+				// Add a ColumnSortEvent.ListHandler to connect sorting to the
+				// java.util.List.
+				ListHandler<User> columnSortHandler = new ListHandler<User>(
+						list);
+				table.addColumnSortHandler(columnSortHandler);
+			}
+
+		});
+
 		class HasCellImpl implements HasCell<User, User> {
 			private ActionCell<User> fCell;
 
@@ -318,8 +327,8 @@ public class AdminPanel extends AbsolutePanel{
 			}
 		};
 		table.addColumn(dateColumn, "Date de derniere connexion");
-*/
-		
+		 */
+
 		TextColumn<User> dateLastColumn = new TextColumn<User>() {
 			@Override
 			public String getValue(User user) {
@@ -327,7 +336,7 @@ public class AdminPanel extends AbsolutePanel{
 			}
 		};
 		table.addColumn(dateLastColumn, "Date de derniere connexion");
-		
+
 		table.addColumn(new Column<User, User>(pseudoColumn) {
 
 			@Override
@@ -348,6 +357,71 @@ public class AdminPanel extends AbsolutePanel{
 		a.isVisible();
 		a.setTitle("Ajouter une salle");
 
+		// Create a table to layout the form options
+		FlexTable layout = new FlexTable();
+		layout.setCellSpacing(6);
+		layout.setWidth("350px");
+		layout.setHeight("450px");
+		FlexCellFormatter cellFormatter = layout.getFlexCellFormatter();
+
+		// Add a title to the form
+		layout.setHTML(0, 0, "<br><i>Il ne vous suffira que de remplir ces infos pour vous créer une nouvelle salle <hr>");
+		cellFormatter.setColSpan(0, 0, 2);
+		cellFormatter.setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
+		final HTML error2HTML = new HTML("");
+
+		// Create TextBox
+		final TextBox nomSalle2Box = new TextBox();
+		final TextBox theme2Box = new TextBox();
+		final TextBox description2Box = new TextBox();
+
+		// Add a drop box with the list types
+		final ListBox nbPlaceBox = new ListBox(false);
+		for (int i = 4; i <= 49; i++) {
+			nbPlaceBox.addItem(""+i+" places");
+		}
+
+		// Add some standard form options
+		layout.setHTML(1, 0, "Nom:");
+		layout.setWidget(1, 1, nomSalle2Box);
+		layout.setHTML(2, 0, "Theme");
+		layout.setWidget(2, 1, theme2Box);
+		layout.setHTML(3, 0, "Description");
+		layout.setWidget(3, 1, description2Box);
+		layout.setHTML(4, 0, "Nombre de places");
+		layout.setWidget(4, 1, nbPlaceBox);
+		layout.setWidget(5, 1, new Button(
+				"Ajouter Salle", new ClickHandler() {
+					public void onClick(ClickEvent event) {
+						if (casesRemplies()){
+							int nbplace = Integer.parseInt(nbPlaceBox.getItemText(nbPlaceBox.getSelectedIndex()).substring(0,2));
+
+							SalleService.Util.getInstance().creerSalle(nomSalle2Box.getText(), theme2Box.getText(),
+									description2Box.getText(),nbplace, new AsyncCallback<Boolean>(){
+								public void onFailure(Throwable caught) {
+									Window.alert("Erreur : "+ caught.getMessage());
+								}
+								public void onSuccess(Boolean result) {
+									if (result==null)
+										Window.alert("Erreur lors de l'ajout de la salle !");
+								}
+							});
+						}else error2HTML.setHTML("<font color=\"#FF00\"><em><small>Erreur : Vous n'avez " +
+						"pas rempli tous les champs !</small></em></font>");
+					}
+
+					private boolean casesRemplies() {
+						return (nomSalle2Box.getText().length()!=0 && 
+								theme2Box.getText().length()!=0 && 
+								description2Box.getText().length()!=0
+						);
+					}
+				}));
+		layout.setWidget(6, 0, error2HTML);
+
+		a.add(layout);
+
+
 		return a;
 	}
 
@@ -363,5 +437,5 @@ public class AdminPanel extends AbsolutePanel{
 		hPanel.add(headerText);
 		return new SimplePanel(hPanel);
 	}
-	
+
 }
